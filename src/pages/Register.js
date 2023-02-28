bc3a3a31b9c607babb930fc40fa2a7a2283e3197
Sanjayvinoth <110889214+Sanjayvinoth7@users.noreply.github.com>
2023-02-28 20:06:34 +0530

    Update Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -89,7 +89,7 @@ export default function Register() {
         <form action="" onSubmit={(event) => handleSubmit(event)}>
           <div className="brand">
             <img src={Logo} alt="logo" />
-            <h1>CHAT WITH ME</h1>
+            <h1>CHAT APP</h1>
           </div>
           <input
             type="text"
